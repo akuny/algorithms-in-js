61f9e2ab7570686be3107a65006e8c0344c2d6f6
akuny <andykuny@gmail.com>
2019-02-28 13:47:30 -0500

    Add additional union find implementations

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // define UnionFind algorithm object
 var UnionFind = function(type) {
   this.length = 10;
+  this.count = this.length;
   this.parentIdStore = [];
+  this.size = [];
   this.type = type;
   this.populateParentIdStore(this.parentIdStore, this.length);
+  this.populateParentIdStore(this.size, this.length);
 };
 
 UnionFind.prototype.populateParentIdStore = function(array, n) {
@@ -13,23 +16,64 @@ UnionFind.prototype.populateParentIdStore = function(array, n) {
 };
 
 UnionFind.prototype.find = function(index) {
-  return this.parentIdStore[index];
+  if (this.type === 'quickFind') {
+    return this.parentIdStore[index];
+  } else if (this.type === 'quickUnion') {
+    while (index !== this.parentIdStore[index]) {
+      index = this.parentIdStore[index];
+    }
+    return index;
+  } else if (this.type === 'weightedQuickUnion') {
+    // weighted quick union implementation
+    while (index !== this.parentIdStore[index]) {
+      index = this.parentIdStore[index];
+    }
+    return index;
+  } else {
+    console.error('error');
+  }
 };
 
 UnionFind.prototype.count = function(type) {
   // return number of components
+  return this.count;
 };
 
-UnionFind.prototype.union = function(type, child, parent) {
-  // change parentId of child to index of parent: make child (p) the child of parent (q)
-  var child = this.find(child);
-  var parent = this.find(parent);
-  if (child === parent) return;
-  for (var i = 0; i < this.parentIdStore.length; i++) {
-    if (this.parentIdStore[i] === child) {
-      this.parentIdStore[i] = parent;
+UnionFind.prototype.union = function(child, parent) {
+  if (this.type === 'quickFind') {
+    // change parentId of child to index of parent: make child (p) the child of parent (q)
+    var child = this.find(child);
+    var parent = this.find(parent);
+    if (child === parent) return;
+    for (var i = 0; i < this.parentIdStore.length; i++) {
+      if (this.parentIdStore[i] === child) {
+        this.parentIdStore[i] = parent;
+      }
     }
+    this.count--;
+  } else if (this.type === 'quickUnion') {
+    // quick union implementation
+    var childRoot = this.find(child);
+    var parentRoot = this.find(parent);
+    if (childRoot === parentRoot) return;
+    this.parentIdStore[childRoot] = parentRoot;
+    this.count--;
+  } else if (this.type === 'weightedQuickUnion') {
+    // weighted quick union implementation
+    var childRoot = this.find(child);
+    var parentRoot = this.find(parent);
+    if (childRoot === parentRoot) return;
+    if (this.size[childRoot] < this.size[parentRoot]) {
+      this.parentIdStore[childRoot] = parentRoot;
+      this.size[parentRoot] = childRoot;
+    } else {
+      this.parentIdStore[parentRoot] = childRoot;
+      this.size[childRoot] = parentRoot;
+    }
+  } else {
+    console.error('error');
   }
+  console.log(this.parentIdStore);
 };
 
 UnionFind.prototype.connected = function(type, indexA, indexB) {
@@ -51,9 +95,11 @@ var App = function() {
   });
 
   this.unionForm.addEventListener('submit', function(event) {
-    var child = document.getElementById('child').value;
-    var parent = document.getElementById('parent').value;
-    self.currentAlgorithm.union(null, child, parent);
+    var child = document.getElementById('child');
+    var parent = document.getElementById('parent');
+    self.currentAlgorithm.union(child.value, parent.value);
+    child.value = '';
+    parent.value = '';
     event.preventDefault();
   });
 };
